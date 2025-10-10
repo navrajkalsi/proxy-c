d3aad74f6d6dcf74e41dba1a8fcfb4b59305f2ca
Navraj Kalsi <navrajkalsi@icloud.com>
2025-10-09 20:44:36 -0400

    init

---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <main.h>
+
+int main(void) {
+  struct addrinfo hints, *out;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET6;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  (void)getaddrinfo("::", "8080", &hints, &out);
+
+  int server_fd = socket(out->ai_family, out->ai_socktype, out->ai_protocol);
+  printf("socket: %d\n", server_fd);
+
+  (void)bind(server_fd, out->ai_addr, out->ai_addrlen);
+  freeaddrinfo(out);
+
+  listen(server_fd, 25);
+
+  int client_fd = accept(server_fd, NULL, NULL);
+  printf("client: %d\n", client_fd);
+
+  char buf[4096];
+
+  int red = read(client_fd, buf, 4096);
+  printf("response:\n%.*s\n", red, buf);
+
+  write(client_fd, "<h1>hello</h1>", strlen("<h1>hello</h1>"));
+
+  close(client_fd);
+
+  return 0;
+}
